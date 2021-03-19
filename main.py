e0e40a5f3b9b1f3d2bf4c43f6068d10bac63c13d
Salvador Guzman <guzmansalv@gmail.com>
2021-03-18 23:22:35 -0700

    put init methods in private class

--- main.py
+++ main.py
@@ -10,48 +10,51 @@ addr: Optional[str] = None
 ela = None
 
 
-def init_elastic() -> None:
-    global ela
-    ela = elasticsearch.Elasticsearch(addr)
+class Init:
+    class PrivateInit:
+        @staticmethod
+        def init_elastic() -> None:
+            global ela
+            ela = elasticsearch.Elasticsearch(addr)
 
+        @staticmethod
+        def init_addr() -> None:
+            global addr
+            addr = sys.argv[1]
+            logging.info('Using address %s', addr)
 
-def init_addr() -> None:
-    global addr
-    addr = sys.argv[1]
-    logging.info('Using address %s', addr)
+        @staticmethod
+        def init_args() -> None:
+            logging.info('Got args:')
+            logging.info(sys.argv)
 
+            Init.PrivateInit.init_addr()
 
-def init_args() -> None:
-    logging.info('Got args:')
-    logging.info(sys.argv)
+        @staticmethod
+        def init_atexit() -> None:
+            def end():
+                logging.info('bye')
 
-    init_addr()
+            atexit.register(end)
 
+        @staticmethod
+        def init_logging() -> None:
+            logging.basicConfig(
+                format='ELASTIC-GRAB %(asctime)s %(levelname)-8s %(message)s',
+                level=logging.INFO,
+                datefmt='%Y-%m-%d %H:%M:%S')
 
-def init_atexit() -> None:
-    def end():
-        logging.info('bye')
+            logging.info('hi')
 
-    atexit.register(end)
-
-
-def init_logging() -> None:
-    logging.basicConfig(
-        format='ELASTIC-GRAB %(asctime)s %(levelname)-8s %(message)s',
-        level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S')
-
-    logging.info('hi')
-
-
-def init() -> None:
-    init_logging()
-    init_atexit()
-    init_args()
+    @staticmethod
+    def init() -> None:
+        Init.PrivateInit.init_logging()
+        Init.PrivateInit.init_atexit()
+        Init.PrivateInit.init_args()
 
 
 def main() -> None:
-    init()
+    Init.init()
 
 
 if __name__ == '__main__':
